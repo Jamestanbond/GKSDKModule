d0277032f7bdde4cc936a16bc1146746d239ac52
luoxin <luoxin3116@gmail.com>
2017-06-13 22:02:11 +0800

    dfa

--- Example/Pods/Target Support Files/TPHomeModule/TPHomeModule-umbrella.h
+++ Example/Pods/Target Support Files/TPHomeModule/TPHomeModule-umbrella.h
@@ -10,6 +10,12 @@
 #endif
 #endif
 
+#import "Label+Button.h"
+#import "UIButton+buttonFastSetting.h"
+#import "UIButton+click.h"
+#import "NSObject+FastInit.h"
+#import "UIView+fastFrameSetting.h"
+#import "UILabel+FastSetting.h"
 
 FOUNDATION_EXPORT double TPHomeModuleVersionNumber;
 FOUNDATION_EXPORT const unsigned char TPHomeModuleVersionString[];
